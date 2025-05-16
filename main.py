673ff2c28d55ac6b0d1b54df56134b426375b8c7
camilo <camimillan2003@gmail.com>
2025-05-15 20:18:40 -0500

    Terminando las demas paginas menos analitica

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
+
+@app.before_request
+def log_request_info():
+    ruta = request.path
+    hora = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(f"[{hora}] Ruta accedida: {ruta}")
 
 @app.route('/')
 def home():
@@ -21,6 +28,10 @@ def clientes():
 @app.route('/contacto')
 def contacto():
     return render_template('contacto.html')
+
+@app.route('/analitica')
+def analitica():
+    return render_template('analitica.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
